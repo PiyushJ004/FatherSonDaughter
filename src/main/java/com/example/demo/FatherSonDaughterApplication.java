2b03437f99144737ebfe285efbffdb96216b3e8d
unknown <pyushjaiswal369@gmail.com>
2020-01-26 00:26:09 +0530

    Updated

--- src/main/java/com/example/demo/FatherSonDaughterApplication.java
+++ src/main/java/com/example/demo/FatherSonDaughterApplication.java
@@ -2,13 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class FatherSonDaughterApplication {
 
-	public static void main(String[] args){
-		ApplicationContext context = SpringApplication.run(FatherSonDaughterApplication.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(FatherSonDaughterApplication.class, args);
 	}
 
 }
